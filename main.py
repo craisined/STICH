create DiscriminatorLoss class, fix softmax in generator and discriminator losses

main.py
@@ -1,6 +1,6 @@
 from dataloader import DataLoader
 import logging
-from models import Generator, Discriminator, GeneratorLoss
+from models import DiscriminatorLoss, Generator, Discriminator, GeneratorLoss
 from pathlib import Path
 import sys
 import torch
@@ -32,10 +32,8 @@ humming_disc.to(device)
 
 classical_to_humming_loss = GeneratorLoss(humming_disc, humming_to_classical_gen)
 humming_to_classical_loss = GeneratorLoss(classical_disc, classical_to_humming_gen)
-# classical_disc_loss = nn.BCEWithLogitsLoss()
-# humming_disc_loss = nn.BCEWithLogitsLoss()
-classical_disc_loss = nn.MSELoss()
-humming_disc_loss = nn.MSELoss()
+classical_disc_loss = DiscriminatorLoss()
+humming_disc_loss = DiscriminatorLoss()
 
 classical_to_humming_optim = torch.optim.SGD(classical_to_humming_gen.parameters(), lr=lr)
 humming_to_classical_optim = torch.optim.SGD(humming_to_classical_gen.parameters(), lr=lr)

models.py
@@ -105,16 +105,19 @@ class GeneratorLoss(nn.Module):
         self.discriminator = discriminator
         self.opposing_generator = opposing_generator
         self.cycle_consistency_factor = cycle_consistency_factor
+
         # self.bce = nn.BCEWithLogitsLoss()
-        self.softmax = nn.Softmax()
+        self.softmax = nn.Softmax(dim=2)
         self.mse = nn.MSELoss()
+        self.l1Loss = nn.L1Loss()
     
     def forward(self, x, original):
         disc_logits = self.discriminator(x)
+        disc_probs = self.softmax(disc_logits)\
+
         # gan_loss = self.bce(disc_logits, torch.ones_like(disc_logits))
-        disc_probs = self.softmax(disc_logits)
         gan_loss = self.mse(disc_probs, torch.ones_like(disc_logits))
-        cycle_consistency_loss = torch.linalg.vector_norm(self.opposing_generator(x) - original)
+        cycle_consistency_loss = self.l1Loss(self.opposing_generator(x), original)
         return gan_loss + self.cycle_consistency_factor * cycle_consistency_loss
 
 class Discriminator(nn.Module):
@@ -146,3 +149,16 @@ class Discriminator(nn.Module):
     def forward(self, x):
         x = self.nn(x)
         return x
+
+class DiscriminatorLoss(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+        # self.bce = nn.BCEWithLogitsLoss()
+        self.softmax = nn.Softmax(dim=2)
+        self.mse = nn.MSELoss()
+    
+    def forward(self, x, original):
+        # return self.bce(x, original)
+        disc_probs = self.softmax(x)
+        return self.mse(disc_probs, original)