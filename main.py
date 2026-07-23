69614348d7428416d7c0adb207f366ca4596fce9
craisin <kahkaicyap@gmail.com>
2026-07-23 14:25:07 -0700

    fix errors

--- main.py
+++ main.py
@@ -1,56 +1,62 @@
-from dataloader import DataLoaderLegacy
+from dataloader import DataLoaderLegacy, HummingClassicalDataset
 import logging
 from models import DiscriminatorLoss, Generator, Discriminator, GeneratorLoss
+import os
 from pathlib import Path
-import sys
+from plotter import Plotter
+
 import torch
 import torch.distributed as dist
 import torch.optim as optim
+from torch.utils.data import DataLoader
 from torch.utils.data.distributed import DistributedSampler
 from torch.nn.parallel import DistributedDataParallel as DDP
-from plotter import Plotter
 
-# Logging
-log_handler = logging.FileHandler("model.log", mode="a", encoding="utf-8")
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s - %(levelname)s - %(message)s",
-    handlers=[log_handler]
-)
-logger = logging.getLogger(__name__)
-
-# GPU
+# GPU & Threads
 dist.init_process_group(backend="nccl")
 local_rank = int(os.environ["LOCAL_RANK"])
 torch.cuda.set_device(local_rank)
+
+# Logging
+if local_rank == 0:
+    log_handler = logging.FileHandler("model.log", mode="a", encoding="utf-8")
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - %(levelname)s - %(message)s",
+        handlers=[log_handler]
+    )
 
 # Datasets
 music_dataset = HummingClassicalDataset(
     humming_dir=Path("data") / "humtrans_processed",
     classical_dir=Path("data") / "musicnet_processed"
 )
+
 disc_sampler = DistributedSampler(music_dataset, shuffle=True)
 disc_dataloader = DataLoader(
     music_dataset, 
-    batch_size=4 * torch.cuda.device_count(),
+    batch_size=4,
     pin_memory=True, 
-    sampler=sampler
+    sampler=disc_sampler
 )
 gen_sampler = DistributedSampler(music_dataset, shuffle=True)
 gen_dataloader = DataLoader(
     music_dataset, 
-    batch_size=4 * torch.cuda.device_count(),
+    batch_size=4,
     pin_memory=True, 
-    sampler=sampler
+    sampler=gen_sampler
 )
 
 # Models
-classical_to_humming_gen, humming_to_classical_gen = Generator(), Generator()
-classical_disc, humming_disc = Discriminator(), Discriminator()
-classical_to_humming_gen.to(local_rank)
-humming_to_classical_gen.to(local_rank)
-classical_disc.to(local_rank)
-humming_disc.to(local_rank)
+classical_to_humming_gen = Generator().to(local_rank)
+humming_to_classical_gen = Generator().to(local_rank)
+classical_disc = Discriminator().to(local_rank)
+humming_disc = Discriminator().to(local_rank)
+
+classical_to_humming_gen = DDP(classical_to_humming_gen, device_ids=[local_rank])
+humming_to_classical_gen = DDP(humming_to_classical_gen, device_ids=[local_rank])
+classical_disc = DDP(classical_disc, device_ids=[local_rank])
+humming_disc = DDP(humming_disc, device_ids=[local_rank])
 
 # Loss and optimizers
 lr = 2e-4
@@ -80,7 +86,8 @@ humming_disc_optim = optim.Adam(
 )
 
 # Plotting
-plotter = Plotter()
+if local_rank == 0:
+    plotter = Plotter()
 classical_disc_loss_history = []
 humming_disc_loss_history = []
 humming_to_classical_gen_loss_history = []
@@ -89,12 +96,20 @@ classical_to_humming_gen_loss_history = []
 epochs = 10
 for epoch in range(epochs):
 
+    disc_sampler.set_epoch(epoch)
+    gen_sampler.set_epoch(epoch)
+
     epoch_classical_disc_loss_history = []
     epoch_humming_disc_loss_history = []
     epoch_humming_to_classical_gen_loss_history = []
     epoch_classical_to_humming_gen_loss_history = []
 
-    for ((humming_disc_data, classical_disc_data), (humming_gen_data, classical_gen_data)) in zip(disc_dataloader, gen_dataloader):   
+    for ((humming_disc_data, classical_disc_data), (humming_gen_data, classical_gen_data)) in zip(disc_dataloader, gen_dataloader): 
+
+        humming_disc_data = humming_disc_data.to(local_rank)
+        classical_disc_data = classical_disc_data.to(local_rank)
+        humming_gen_data = humming_gen_data.to(local_rank)
+        classical_gen_data = classical_gen_data.to(local_rank)  
 
         classical_disc_optim.zero_grad()
         humming_disc_optim.zero_grad()
@@ -103,27 +118,29 @@ for epoch in range(epochs):
         classical_output = humming_to_classical_gen(humming_disc_data).detach()
         classical_probs = classical_disc(classical_output)
         classical_loss_val = classical_disc_loss(classical_probs, torch.zeros_like(classical_probs))
-        logger.info(f"Loss for classical discriminator (fake): {classical_loss_val}")
         classical_loss_val.backward()
 
         humming_output = classical_to_humming_gen(classical_disc_data).detach()
         humming_probs = humming_disc(humming_output)
         humming_loss_val = humming_disc_loss(humming_probs, torch.zeros_like(humming_probs))
-        logger.info(f"Loss for humming discriminator (fake): {humming_loss_val}")
         humming_loss_val.backward()
+
+        if local_rank == 0:
+            logger.info(f"Loss for discriminators (fake): {classical_loss_val} (classical) | {humming_loss_val} (humming)")
 
         # Train discriminator with real data
         classical_probs = classical_disc(classical_disc_data)
         classical_loss_val = classical_disc_loss(classical_probs, torch.ones_like(classical_probs))
-        logger.info(f"Loss for classical discriminator (real): {classical_loss_val}")
         epoch_classical_disc_loss_history.append(classical_loss_val.item())
         classical_loss_val.backward()
 
         humming_probs = humming_disc(humming_disc_data)
         humming_loss_val = humming_disc_loss(humming_probs, torch.ones_like(humming_probs))
-        logger.info(f"Loss for humming discriminator (real): {humming_loss_val}")
         epoch_humming_disc_loss_history.append(humming_loss_val.item())
         humming_loss_val.backward()
+
+        if local_rank == 0:
+            logger.info(f"Loss for discriminators (real): {classical_loss_val} (classical) | {humming_loss_val} (humming)")
 
         classical_disc_optim.step()
         humming_disc_optim.step()
@@ -134,37 +151,40 @@ for epoch in range(epochs):
 
         classical_output = humming_to_classical_gen(humming_gen_data)
         classical_loss_val = humming_to_classical_loss(classical_output, humming_gen_data)
-        logger.info(f"Loss for classical generator: {classical_loss_val}")
         epoch_humming_to_classical_gen_loss_history.append(classical_loss_val.item())
         classical_loss_val.backward()
 
         humming_output = classical_to_humming_gen(classical_gen_data)
         humming_loss_val = classical_to_humming_loss(humming_output, classical_gen_data)
-        logger.info(f"Loss for humming generator: {humming_loss_val}")
         epoch_classical_to_humming_gen_loss_history.append(humming_loss_val.item())
         humming_loss_val.backward()
 
+        if local_rank == 0:
+            logger.info(f"Loss for generators: {classical_loss_val} (classical) | {humming_loss_val} (humming)")
+
         classical_to_humming_optim.step()
         humming_to_classical_optim.step()
-        
-    plotter.plotEpochLoss(
-        epoch + 1, 
-        epoch_classical_disc_loss_history, 
-        epoch_humming_disc_loss_history, 
-        epoch_humming_to_classical_gen_loss_history, 
-        epoch_classical_to_humming_gen_loss_history
-    )
+    
+    if local_rank == 0:
+        plotter.plotEpochLoss(
+            epoch + 1, 
+            epoch_classical_disc_loss_history, 
+            epoch_humming_disc_loss_history, 
+            epoch_humming_to_classical_gen_loss_history, 
+            epoch_classical_to_humming_gen_loss_history
+        )
     
     classical_disc_loss_history.append(sum(epoch_classical_disc_loss_history) / len(epoch_classical_disc_loss_history))
     humming_disc_loss_history.append(sum(epoch_humming_disc_loss_history) / len(epoch_humming_disc_loss_history))
     humming_to_classical_gen_loss_history.append(sum(epoch_humming_to_classical_gen_loss_history) / len(epoch_humming_to_classical_gen_loss_history))
     classical_to_humming_gen_loss_history.append(sum(epoch_classical_to_humming_gen_loss_history) / len(epoch_classical_to_humming_gen_loss_history))
-    
-plotter.plotFullLoss(
-    classical_disc_loss_history,
-    humming_disc_loss_history,
-    humming_to_classical_gen_loss_history,
-    classical_to_humming_gen_loss_history
-)
+
+if local_rank == 0:
+    plotter.plotFullLoss(
+        classical_disc_loss_history,
+        humming_disc_loss_history,
+        humming_to_classical_gen_loss_history,
+        classical_to_humming_gen_loss_history
+    )
 
 dist.destroy_process_group()
