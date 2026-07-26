7cf0247addb61cfddc0fb87b849e2d55277b4bc2
craisin <kahkaicyap@gmail.com>
2026-07-26 13:37:21 -0700

    fix logging and add batch size flag

--- main.py
+++ main.py
@@ -18,10 +18,12 @@ from torch.nn.parallel import DistributedDataParallel as DDP
 
 def parse_args():
     parser = argparse.ArgumentParser(allow_abbrev=False)
-    parser.add_argument("--epochs", type=int, default=10)
+    parser.add_argument("--epochs", type=int, default=20)
     parser.add_argument("--lr", type=float, default=2e-4)
-    parser.add_argument("--cycle-factor", type=int, default=10)
+    parser.add_argument("--cycle-factor", type=int, default=2)
+    parser.add_argument("--batch-size", type=int, default=16)
     return parser.parse_args()
+
 
 def main():
 
@@ -58,7 +60,7 @@ def main():
 
     sampler = DistributedSampler(music_dataset, shuffle=True)
     dataloader = DataLoader(
-        music_dataset, batch_size=16, pin_memory=True, sampler=sampler, num_workers=1
+        music_dataset, batch_size=args.batch_size, pin_memory=True, sampler=sampler, num_workers=1
     )
 
     # Models
@@ -133,6 +135,7 @@ def main():
                 classical_loss_val = classical_disc_loss(
                     classical_probs, torch.zeros_like(classical_probs)
                 )
+                classical_fake_disc_loss = classical_loss_val.item()
             scaler.scale(classical_loss_val).backward()
 
             with autocast("cuda"):
@@ -141,6 +144,7 @@ def main():
                 humming_loss_val = humming_disc_loss(
                     humming_probs, torch.zeros_like(humming_probs)
                 )
+                humming_fake_disc_loss = humming_loss_val.item()
             scaler.scale(humming_loss_val).backward()
 
             # Train discriminator with real data
@@ -149,7 +153,10 @@ def main():
                 classical_loss_val = classical_disc_loss(
                     classical_probs, torch.ones_like(classical_probs)
                 )
-            epoch_classical_disc_loss_history.append(classical_loss_val.item())
+                classical_real_disc_loss = classical_loss_val.item()
+            epoch_classical_disc_loss_history.append(
+                (classical_real_disc_loss + classical_fake_disc_loss) // 2
+            )
             scaler.scale(classical_loss_val).backward()
 
             with autocast("cuda"):
@@ -157,7 +164,10 @@ def main():
                 humming_loss_val = humming_disc_loss(
                     humming_probs, torch.ones_like(humming_probs)
                 )
-            epoch_humming_disc_loss_history.append(humming_loss_val.item())
+                humming_real_disc_loss = humming_loss_val.item()
+            epoch_humming_disc_loss_history.append(
+                (humming_real_disc_loss + humming_fake_disc_loss) // 2
+            )
             scaler.scale(humming_loss_val).backward()
 
             scaler.step(classical_disc_optim)
@@ -172,9 +182,8 @@ def main():
                 classical_loss_val = humming_to_classical_loss(
                     classical_output, humming_data
                 )
-            epoch_humming_to_classical_gen_loss_history.append(
-                classical_loss_val.item()
-            )
+                classical_gen_loss = classical_loss_val.item()
+            epoch_humming_to_classical_gen_loss_history.append(classical_gen_loss)
             scaler.scale(classical_loss_val).backward()
 
             with autocast("cuda"):
@@ -182,18 +191,19 @@ def main():
                 humming_loss_val = classical_to_humming_loss(
                     humming_output, classical_data
                 )
-            epoch_classical_to_humming_gen_loss_history.append(humming_loss_val.item())
+                humming_gen_loss = humming_loss_val.item()
+            epoch_classical_to_humming_gen_loss_history.append(humming_gen_loss)
             scaler.scale(humming_loss_val).backward()
 
             if local_rank == 0 and iteration % iters_per_log == 0:
                 logger.info(
-                    f"Loss for discriminators (fake): {classical_loss_val} (classical) | {humming_loss_val} (humming)"
+                    f"Loss for discriminators (fake): {classical_fake_disc_loss} (classical) | {humming_fake_disc_loss} (humming)"
                 )
                 logger.info(
-                    f"Loss for discriminators (real): {classical_loss_val} (classical) | {humming_loss_val} (humming)"
+                    f"Loss for discriminators (real): {classical_real_disc_loss} (classical) | {humming_real_disc_loss} (humming)"
                 )
                 logger.info(
-                    f"Loss for generators: {classical_loss_val} (classical) | {humming_loss_val} (humming)"
+                    f"Loss for generators: {classical_gen_loss} (classical) | {humming_gen_loss} (humming)"
                 )
 
             scaler.step(classical_to_humming_optim)
