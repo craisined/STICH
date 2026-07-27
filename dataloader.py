5284d670943cf152791b42302aa66e60401898ed
craisin <kahkaicyap@gmail.com>
2026-07-26 19:27:36 -0700

    fix dataloader shape

--- dataloader.py
+++ dataloader.py
@@ -7,56 +7,6 @@ from torch.utils.data import Dataset
 
 logger = logging.getLogger(__name__)
 
-class DataLoaderLegacy:
-
-    def __init__(self, humming_dir, classical_dir, device=None):
-
-        self.device = device
-        self.humming_files = list(Path(humming_dir).glob("*.npy"))
-        self.classical_files = list(Path(classical_dir).glob("*.npy"))
-        self.humming_files_len = len(self.humming_files)
-        self.classical_files_len = len(self.classical_files)
-
-        assert self.humming_files_len > self.classical_files_len # not great practice, but who's gonna stop me!
-        assert self.classical_files_len > 0
-
-        self.reset()
-
-    def _load(self, path):
-        array = np.load(path).astype(np.float32)
-        tensor = torch.from_numpy(array) # TODO: check if shape is correct
-        tensor = tensor.reshape((1, 1, -1))
-        return tensor.to(self.device)
-    
-    def reset(self):
-        random.shuffle(self.humming_files)
-        random.shuffle(self.classical_files)
-        self.humming_pos = 0 
-        self.classical_pos = 0
-
-    def pop(self): 
-        humming = self.crop(self._load(self.humming_files[self.humming_pos]))
-        classical = self.crop(self._load(self.classical_files[self.classical_pos]))
-
-        self.humming_pos += 1
-        self.classical_pos += 1
-
-        if self.humming_pos >= self.humming_files_len:
-            self.reset()
-        elif self.classical_pos >= self.classical_files_len:
-            random.shuffle(self.classical_files)
-            self.classical_pos = 0
-
-        return humming, classical
-    
-    def crop(self, sample):
-        original_length = sample.shape[2]
-        remainder = original_length % 4
-        cropped_length = original_length - remainder
-        cropped_sample = sample[:, :, :cropped_length]
-        
-        return cropped_sample
-
 class HummingClassicalDataset(Dataset):
 
     def __init__(self, humming_dir, classical_dir):
@@ -90,7 +40,7 @@ class HummingClassicalDataset(Dataset):
 
 def load(path):
     array = np.load(path).astype(np.float32)
-    tensor = torch.from_numpy(array).reshape(1, -1)  
+    tensor = torch.from_numpy(array.reshape(1, 128, 1251))
     return crop(tensor).contiguous()
 
 def crop(sample):
