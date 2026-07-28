a0ab371a737d2a0298ca62ac3d284c9397d7e275
craisin <kahkaicyap@gmail.com>
2026-07-28 14:44:01 -0700

    generate embedding

--- autoencoder.py
+++ autoencoder.py
@@ -1,5 +1,7 @@
 import torch
 import torchaudio
+from pathlib import Path
+import numpy as np
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -7,17 +9,22 @@ model_path = "musicnet.ts"
 model = torch.jit.load(model_path, map_location=device)
 model.eval()
 
-def generate_humming_embedding():
-    pass
+def generate_embedding(folder):
+    vector_sum = 0
+    count = 0
+    for file in folder.iterdir():
+        audio_data = torch.from_numpy(np.load(file)).reshape(1, 1, -1)
+        with torch.no_grad():
+            vector_sum += model.encode(audio_data)
+        count += 1
+    return vector_sum / count
 
-def generate_classical_embedding():
-    pass
-
-humming_embedding = generate_humming_embedding()
-classical_embedding = generate_classical_embedding()
+humming_embedding = generate_embedding()
+classical_embedding = generate_embedding()
 
 def humming_to_classical_embedding(audio_embedding):
     return audio_embedding - humming_embedding + classical_embedding
 
-def classical_to_humming_embedding(audio):
+def classical_to_humming_embedding(audio_embedding):
     return audio_embedding - classical_embedding + humming_embedding
+
