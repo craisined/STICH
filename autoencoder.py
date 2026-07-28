5e4adfdedb17d591ee3a0f3495853cdd9fade328
craisin <kahkaicyap@gmail.com>
2026-07-28 14:22:51 -0700

    boilerplate

---
+++ autoencoder.py
@@ -0,0 +1,15 @@
+
+def generate_humming_embedding():
+    pass
+
+def generate_classical_embedding():
+    pass
+
+humming_embedding = generate_humming_embedding()
+classical_embedding = generate_classical_embedding()
+
+def humming_to_classical(audio):
+    pass
+
+def classical_to_humming(audio):
+    pass
