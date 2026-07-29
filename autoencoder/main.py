a7358217199708c49d8a12199104ab7606a251a4
craisin <kahkaicyap@gmail.com>
2026-07-29 11:00:28 -0700

    discriminator

--- autoencoder/main.py
+++ autoencoder/main.py
@@ -2,9 +2,9 @@ class ResNet(nn.Module):
     def __init__(self):
         super().__init__()
         self.network = nn.Sequential(
-            nn.Conv1d(),
+            nn.Conv1d(padding="same", kernel=25, in_channels=64, out_channels=64),
             nn.ReLU(),
-            nn.Conv1d()
+            nn.Conv1d(padding="same", kernel=25, in_channels=64, out_channels=64)
         )
 
     def forward(self, inp):
@@ -16,11 +16,32 @@ class Generator(nn.Module):
         super().__init__()
         self.network = nn.Sequential(
             ResNet(),
+            nn.ReLU(),
             ResNet(),
+            nn.ReLU(),
             ResNet(),
+            nn.ReLU(),
             ResNet(),
+            nn.ReLU(),
             ResNet(),
+            nn.ReLU(),
         )
 
-    def forward(self):
-        pass
+    def forward(self, inp):
+        return self.network(inp)
+
+class Discriminator(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.network = nn.Sequential(
+            nn.Linear(16, 243),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+            nn.Linear(256, 64),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+            nn.Linear(64, 1),
+        )
+
+    def forward(self, x):
+        return self.network(x)
